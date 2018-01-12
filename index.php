a595c99c946dd88011da76fc3f55704bca8cc249
Scott Medlock <smedlock@mail.greenriver.edu>
2018-01-12 17:47:57 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Set debug level
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+    $view = new View;
+    echo $view->render('pages/home.html');
+}
+);
+
+//Run Fat-Free
+$f3->run();
