e2c3315b4cf536f4669805ef1d2d78abeffcfb2a
Scott Medlock <smedlock@mail.greenriver.edu>
2018-02-17 01:41:33 -0500

    added php doc blocks to Member and PremiumMember

--- classes/Member.php
+++ classes/Member.php
@@ -1,3 +1,5 @@
+
+
 <?php
 /**
  * Created by PhpStorm.
@@ -6,6 +8,15 @@
  * Time: 2:15 PM
  */
 
+/**
+ * Class Member represents a member of the dating site
+ *
+ * This will store a member's first name, last name, age, gender, phone number,
+ * email address, state, the gender they're seeking, and their biography.
+ *
+ * @author Scott Medlock
+ * @copyright 2018
+ */
 class Member
 {
     protected $fname;
@@ -18,7 +29,21 @@ class Member
     protected $seeking;
     protected $bio;
 
-    function __construct($fname, $lname, $age, $gender, $phone, $email, $state, $seeking, $bio)
+    /**
+     * Member constructor.
+     *
+     * @param $fname first name
+     * @param $lname last name
+     * @param $age age
+     * @param $gender gender
+     * @param $phone phone number
+     * @param $email email address
+     * @param $state state
+     * @param $seeking seeking male or female
+     * @param $bio biography
+     */
+    function __construct($fname, $lname, $age, $gender,$phone,
+                                $email, $state, $seeking, $bio)
     {
         $this->fname = $fname;
         $this->lname = $lname;
@@ -31,91 +56,181 @@ class Member
         $this->bio = $bio;
     }
 
+    /**
+     * gets the first name of the member
+     *
+     * @return fname first name of the member
+     */
     public function getFName()
     {
         return $this->fname;
     }
 
+    /**
+     * sets the first name of the member
+     *
+     * @param $fname the first name to update the member to
+     */
     public function setFName($fname)
     {
         $this->fname = $fname;
     }
 
+    /**
+     * gets the last name of the member
+     *
+     * @return lname last name of the member
+     */
     public function getLName()
     {
         return $this->lname;
     }
 
+    /**
+     * sets the last name of the member
+     *
+     * @param $lname the last name to update the member to
+     */
     public function setLName($lname)
     {
         $this->lname = $lname;
     }
 
+    /**
+     * gets the age of the member
+     *
+     * @return age the age of the member
+     */
     public function getAge()
     {
         return $this->age;
     }
 
+    /**
+     * sets the age of the member
+     *
+     * @param $age the new age to update the member to
+     */
     public function setAge($age)
     {
         $this->age = $age;
     }
 
+    /**
+     * gets the gender of the member
+     *
+     * @return gender the gender of the member
+     */
     public function getGender()
     {
         return $this->gender;
     }
 
+    /**
+     * sets the gender of the member
+     *
+     * @param $gender the gender to update the member to
+     */
     public function setGender($gender)
     {
         $this->gender = $gender;
     }
 
+    /**
+     * gets the phone number of the member
+     *
+     * @return phone the phone number of the member
+     */
     public function getPhone()
     {
         return $this->phone;
     }
 
+    /**
+     * sets the phone number of the member.
+     *
+     * @param $phone the phone number to update for the member
+     */
     public function setPhone($phone)
     {
         $this->phone = $phone;
     }
 
+    /**
+     * gets the email address of the member
+     *
+     * @return email the email address of the member
+     */
     public function getEmail()
     {
         return $this->email;
     }
 
+    /**
+     * sets the email address for the member
+     *
+     * @param $email the email address to update for the member
+     */
     public function setEmail($email)
     {
         $this->email = $email;
     }
 
+    /**
+     * Gets the state the member is from
+     *
+     * @return state the state the member is from
+     */
     public function getState()
     {
         return $this->state;
     }
 
+    /**
+     * sets the state for the member
+     *
+     * @param $state the state to update for the member
+     */
     public function setState($state)
     {
         $this->state = $state;
     }
 
+    /**
+     * gets the gender that the member is seeking
+     *
+     * @return seeking the gender that the member is seeking
+     */
     public function getSeeking()
     {
         return $this->seeking;
     }
 
+    /**
+     * sets the gender that the member is seeking
+     *
+     * @param $seeking the gender that the member is seeking
+     */
     public function setSeeking($seeking)
     {
         $this->seeking = $seeking;
     }
 
+    /**
+     * gets the biography of the member
+     *
+     * @return bio the biography of this member
+     */
     public function getBio()
     {
         return $this->bio;
     }
 
+    /**
+     * sets the biography of the member
+     *
+     * @param $bio the biography to set for the member
+     */
     public function setBio($bio)
     {
         $this->bio = $bio;
--- classes/PremiumMember.php
+++ classes/PremiumMember.php
@@ -6,30 +6,80 @@
  * Time: 2:40 PM
  */
 
+/**
+ * Class PremiumMember represents a member of the dating site with premium
+ * attributes
+ *
+ * In addition to the member's first name, last name, age, gender,
+ * phone number, email address, state, the gender they're seeking,
+ * and their biography, PremiumMembers also store indoor and
+ * outdoor interests.
+ *
+ * @author Scott Medlock
+ * @copyright 2018
+ */
 class PremiumMember extends Member
 {
     private $_inDoorInterests;
     private $_outDoorInterests;
 
-    function __construct($fname, $lname, $age, $gender, $phone, $email, $state, $seeking, $bio, $inDoorInterests, $outDoorInterests)
+
+    /**
+     * PremiumMember constructor.
+     *
+     * @param $fname first name
+     * @param $lname last name
+     * @param $age age
+     * @param $gender gender
+     * @param $phone phone number
+     * @param $email email address
+     * @param $state state
+     * @param $seeking seeking male or female
+     * @param $bio biography
+     * @param $inDoorInterests indoor interests
+     * @param $outDoorInterests outdoor interests
+     */
+    function __construct($fname, $lname, $age, $gender, $phone, $email, $state,
+                         $seeking, $bio, $inDoorInterests, $outDoorInterests)
     {
-        parent::__construct($fname, $lname, $age, $gender, $phone, $email, $state, $seeking, $bio);
+        parent::__construct($fname, $lname, $age, $gender, $phone,
+                                    $email, $state, $seeking, $bio);
         $this->_inDoorInterests = $inDoorInterests;
         $this->_outDoorInterests = $outDoorInterests;
     }
 
+    /**
+     * gets the indoor interests of the premium member
+     *
+     * @return inDoorInterests indoor interests of the premium member
+     */
     public function getInDoorInterests() {
         return $this->_inDoorInterests;
     }
 
+    /**
+     * sets the indoor interests of the premium member
+     *
+     * @param $inDoorInterests indoor interests of the premium member
+     */
     public function setInDoorInterests($inDoorInterests) {
         $this->_inDoorInterests = $inDoorInterests;
     }
 
+    /**
+     * gets the outdoor interests of the premium member
+     *
+     * @return outDoorInterests outdoor interests of the premium member
+     */
     public function getOutDoorInterests() {
         return $this->_outDoorInterests;
     }
 
+    /**
+     * sets the outdoor interests of the premium member
+     *
+     * @param $outDoorInterests outdoor interests of the premium member
+     */
     public function setOutDoorInterests($outDoorInterests) {
         $this->_outDoorInterests = $outDoorInterests;
     }
